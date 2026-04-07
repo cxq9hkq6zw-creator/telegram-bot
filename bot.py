fa7db23eb499aa7c65f2587fcd5893026f0fd72e
cxq9hkq6zw-creator <itayaviv1010@gmail.com>
2026-04-07 17:35:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,29 +3,25 @@ import asyncio
 import re
 import os
 
-# 🔐 משתנים מ-Render (Environment Variables)
 api_id = "39189498"
 api_hash = "f457cf80ff6f8123dba1492bc4cf25ca"
-bot_token = "8696135908:AAH81vCUOCTH6UGXPS7hlAjEpBr3b1dIMuA"
+bot_token = ״8696135908:AAH81vCUOCTH6UGXPS7hlAjEpBr3b1dIMuA
 
-client = TelegramClient("session", api_id, api_hash)
+client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
 
-source_channel = "pkpoi"
+source_channel = "@rakemett"
 target_channel = "itay_alerts"
 
 @client.on(events.NewMessage(chats=source_channel))
 async def handler(event):
     text = event.message.text or ""
 
-    # ❌ מוחק פרסומות
     if "הישארו מעודכנים" in text:
         text = text.split("הישארו מעודכנים")[0]
 
-    # 🧼 ניקוי טקסט
     text = re.sub(r'[*_`]', '', text)
     text = text.replace("❗", "").strip()
 
-    # ✅ טקסט סופי
     final_text = f"{text}\n\n🔴 חדשות רק אמת בטלגרם 🔴\nhttps://t.me/itay_alerts"
 
     try:
@@ -42,12 +38,10 @@ async def handler(event):
                 final_text,
                 link_preview=False
             )
-
     except Exception as e:
         print("❌ שגיאה:", e)
 
 async def main():
-    await client.start()
     print("🚀 הבוט עובד!")
     await client.run_until_disconnected()
 
