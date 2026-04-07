5b5e64ed7e5edb36c40463f99c8941d7049bc211
cxq9hkq6zw-creator <itayaviv1010@gmail.com>
2026-04-07 18:58:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot_token = "8696135908:AAH81vCUOCTH6UGXPS7hlAjEpBr3b1dIMuA"
 
 client = TelegramClient("bot", api_id, api_hash)
 
-source_channel = "pkpoi"
+source_channel = "itay_news"
 target_channel = "@itay_alerts"
 
 @client.on(events.NewMessage(chats=source_channel))
